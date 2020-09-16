2dcc101a1110163cb52db218df370b8c976697f7
KocherginV <volodymyrkochergin@gmail.com>
2020-09-16 16:17:21 +0200

    add more tests in page object

--- test/page/main.page.js
+++ test/page/main.page.js
@@ -3,8 +3,36 @@ class MainPage {
     get searchInput() {
         return $('#search_input_react')
     }
+
+    get docsNavBarButton() {
+        return $('[class="nav-site nav-site-internal"] [href="/docs/gettingstarted.html"]')
+    }
+
+    get apiNavBarButton() {
+        return $('[class="nav-site nav-site-internal"] [href="/docs/api.html"]')
+    }
+
+    get helpNavBarButton() {
+        return $('[class="nav-site nav-site-internal"] [href="/help.html"]')
+    }
+
+    get versionsNavBarButton() {
+        return $('[class="nav-site nav-site-internal"] [href="/docs/versions.html"]')
+    }
     
-    setUserName(value) {
+    get blogNavBarButton() {
+        return $('[class="nav-site nav-site-internal"] [href="/blog/"]')
+    }
+
+    get contributeNavBarButton() {
+        return $('[class="nav-site nav-site-internal"] [href="/docs/contribute.html"]')
+    }
+
+    get getStartedButton() {
+        return $('[class="pluginWrapper buttonWrapper"] [href="/docs/gettingstarted.html"]')
+    }
+
+    setValueSearch(value) {
         this.searchInput.addValue(value)
     }
 
--- test/specs/basic.js
+++ test/specs/basic.js
@@ -4,10 +4,68 @@ const mainPage = require('../page/main.page.js')
 describe('webdriverio page', () => {
     it('should demonstrate addValue command', () => {
         browser.url('https://webdriver.io');
-        browser.pause(5000)
+        browser.pause(2000);
         mainPage.searchInput.setValue('test');
         browser.pause(2000);
-        mainPage.setUserName('type with method');
+        mainPage.setValueSearch('type with method');
         browser.pause('2000');
     })
+
+    it('should click on Docs button', () => {
+        browser.url('https://webdriver.io');
+        browser.pause(2000);
+        mainPage.docsNavBarButton.click()
+        browser.pause(2000);
+        assert(browser.getUrl() == 'https://webdriver.io/docs/gettingstarted.html');
+    })
+
+    it('should click on API button', () => {
+        browser.url('https://webdriver.io');
+        browser.pause(2000);
+        mainPage.apiNavBarButton.click();
+        browser.pause(2000);
+        assert(browser.getUrl() == 'https://webdriver.io/docs/api.html');
+    })
+
+    it('should click on Help button', () => {
+        browser.url('https://webdriver.io');
+        browser.pause(2000);
+        mainPage.helpNavBarButton.click();
+        browser.pause(2000);
+        assert(browser.getUrl() == 'https://webdriver.io/help.html');
+    })
+    
+    it('should click on Versions button', () => {
+        browser.url('https://webdriver.io');
+        browser.pause(2000);
+        mainPage.versionsNavBarButton.click();
+        browser.pause(2000);
+        assert(browser.getUrl() == 'https://webdriver.io/docs/versions.html');
+    })
+
+    it('should click on Blog button', () => {
+        browser.url('https://webdriver.io');
+        browser.pause(2000);
+        mainPage.blogNavBarButton.click();
+        browser.pause(2000);
+        assert(browser.getUrl() == 'https://webdriver.io/blog/');
+    })
+
+    it('should click on Contribute button', () => {
+        browser.url('https://webdriver.io');
+        browser.pause(2000);
+        mainPage.contributeNavBarButton.click();
+        browser.pause(2000);
+        assert(browser.getUrl() == 'https://webdriver.io/docs/contribute.html');
+    })
+    
+    it('should click on Get Started button', () => {
+        browser.url('https://webdriver.io');
+        browser.pause(2000);
+        expect(mainPage.getStartedButton).toBeDisplayed();
+        mainPage.getStartedButton.click();
+        browser.pause(2000);
+        assert(browser.getUrl() == 'https://webdriver.io/docs/gettingstarted.html');
+    })
+
 })
